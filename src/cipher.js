98ed7c002982e97fc024f82420d1e2d17a315067
brendapsd <brenda.sd.15@gmail.com>
2018-11-19 00:45:54 -0500

    cambios en el index.html y cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,10 @@
-window.cipher = {
-  // ... 
-};
+// window.cipher = {
+  // ...
+ let getData = function () {
+    let texto = document.getElementById("texto").value;
+    console.log(texto);
+    document.getElementById("texto").value = "";
+    document.getElementById("textoCifrado").value = texto;
+
+  };
+// };
