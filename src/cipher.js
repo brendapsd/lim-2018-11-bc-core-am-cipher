4a3852297d4a74ccb61732282092d9acbeb8cf6c
brendapsd <brenda.sd.15@gmail.com>
2018-11-20 00:01:11 -0500

    cambios en las funciones de cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -8,47 +8,15 @@
 
   };*/
 // };
+ const textoCifrado = document.getElementById('texto-cifrado');
+ const btnCifrar = document.getElementById('botonCifrar');
 
- const texto = document.getElementById('texto');
- const textoCifrado = document.getElementById('textoCifrado');
-const btnCifrar = document.getElementById('btnCifrar');
-const btnDescifrar = document.getElementById('btnDescifrar');
-const offset = document.getElementById('offset');
+const moverTexto = () => {
+  const textoACifrar = document.getElementById('textoUsuario').value;
+  return textoACifrar;
 
+}
 
 btnCifrar.addEventListener('click', () => {
-    document.getElementById("texto").value = "";
-    document.getElementById("textoCifrado").value = texto;
+    textoCifrado.innerHTML = moverTexto();
 });
-
-
-
-/* const sumResponse = document.getElementById('sum-response');
-const btnResponse = document.getElementById('btn-response');
-
-const sum = (num1, num2) => {
-  const number1 = parseInt(num1);
-  const number2 = parseInt(num2);
-
-  return number1 + number2;
-};
-
-btnResponse.addEventListener('click', () => {
-  const num1 = document.getElementById('num1').value;
-  const num2 = document.getElementById('num2').value;
-  sumResponse.innerHTML = sum(num1, num2);
-}); 
-
-const phraseToVowels = (text) => {
-  for (let i = 0; i < text.length; i++) {
-    return text[i];
-  }
-};
-
- const btnText = document.getElementById('btn-text');
- const letter = document.getElementById('letter');
-
- btnText.addEventListener('click', () => {
-   const textName = document.getElementById('text-name').value;
-   letter.innerHTML = phraseToVowels(textName);
- }); */
