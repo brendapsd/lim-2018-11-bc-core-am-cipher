7e6bf820c62ef9210f9ac18ae5c96cfeb1ad29f7
brendapsd <brenda.sd.15@gmail.com>
2018-12-03 13:07:52 -0500

    cambios en el style.css

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 
-const btnCifrar = document.getElementById('botonCifrar');
-const btnDescifrar = document.getElementById('botonDescifrar');
-let btnCopy = document.getElementById('btn-copy');
+const btnCifrar = document.getElementById('boton-cifrar');
+const btnDescifrar = document.getElementById('boton-descifrar');
+const btnCopy = document.getElementById('btn-copy');
 
   btnCifrar.addEventListener('click',()=>{
-    const stringMessage = document.getElementById('textoIngresado').value;
-    const offsetFinal = parseInt(document.getElementById('NumOffset').value);
+    const stringMessage = document.getElementById('texto-ingresado').value;
+    const offsetFinal = parseInt(document.getElementById('num-offset').value);
     const resultadoCipher = cipher.encode(offsetFinal,stringMessage);
-    document.getElementById("cifrado").innerHTML = resultadoCipher;
+    document.getElementById('texto-cifrado').innerHTML = resultadoCipher;
   });
 
   btnDescifrar.addEventListener('click',()=>{
-    const stringMessage2 = document.getElementById('textoIngresado').value;
-    const offsetFinal2 = parseInt(document.getElementById('NumOffset').value);
+    const stringMessage2 = document.getElementById('texto-ingresado').value;
+    const offsetFinal2 = parseInt(document.getElementById('num-offset').value);
     const resultadoDecipher = cipher.decode(offsetFinal2,stringMessage2);
-    document.getElementById("cifrado").innerHTML = resultadoDecipher;
+    document.getElementById('texto-cifrado').innerHTML = resultadoDecipher;
   });
 
   btnCopy.addEventListener('click', () => {
-     let aux = document.getElementById('cifrado');
+     let aux = document.getElementById('texto-cifrado');
      aux.select();
      // Copia el texto seleccionado
      document.execCommand("copy");
   });
+
